14d63775869a5d726a2184fdaa154b559f06f9db
ForgQi <34411314+ForgQi@users.noreply.github.com>
2022-01-24 03:03:41 +0800

    Update struct

--- README.md
+++ README.md
@@ -1,18 +1,19 @@
 # Biliup
+基于TAURI: ` GUI: Svelte , 后端: Rust`
 
 bilibili投稿客户端，理论上支持Windows，Linux，macOS。
 
 主要是为了解决现有网页端不能多p投稿的问题，虽然现有b站客户端可以多p
 但是有几个问题：
-* 不支持多平台
+* 不支持多平台，限制单视频大小4G以内
 * 投稿线路对国外不友好，速度较慢稳定性较差
 * 不能批量选择文件，多p只能一个一个文件选择
 
-[下载地址](https://github.com/ForgQi/Caution/releases/download/v0.0.1/biliup.exe)
+[下载地址](https://github.com/ForgQi/Caution/releases)
 
 ---
-![login](biliup-img/login.png)
-![main](biliup-img/main.png)
+<!-- ![login](.github/resource/login.png) -->
+![main](.github/resource/main.png)
 
 ## Roadmap
 - [ ] 短信登录
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,11 +5,16 @@
 
 use anyhow::{anyhow, bail, Context};
 use app::error::Result;
-use app::video::{BiliBili, Client, LoginInfo, Studio, Video};
-use app::{Account, Config, User};
+// use app::video::{BiliBili, Client, LoginInfo, Studio, Video};
+// use app::{Account, Config, User};
+use biliup::{line, Config, User, Account};
 use std::cell::Cell;
+use std::path::PathBuf;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
+use std::time::Instant;
+use biliup::client::{Client, LoginInfo};
+use biliup::video::{BiliBili, Studio, Video};
 use tauri::{Manager, Window};
 
 #[tauri::command]
@@ -59,69 +64,69 @@ async fn upload(mut video: Video, window: Window) -> Result<(Video, f64)> {
     let login_info = client
         .login_by_cookies(std::fs::File::open("cookies.json")?)
         .await?;
-    let bili = BiliBili::new((client, login_info));
+    // let bili = BiliBili::new(login_info, client);
     // let videos = &self.studio.videos;
     // let mut new_videos = Vec::with_capacity(videos.len());
     // if studio.videos.is_empty() { return Err(app::error::Error::Err("文件不能为空".into())) }
     // for video in &mut studio.videos {
-    let remove = Arc::new(AtomicBool::new(false));
+    let remove = Arc::new(AtomicBool::new(true));
     let is_remove = Arc::clone(&remove);
     let id = window.once(&video.filename, move |event| {
         println!("got window event-name with payload {:?}", event.payload());
-        is_remove.store(true, Ordering::Relaxed);
+        is_remove.store(false, Ordering::Relaxed);
     });
-    let mut total_size = 0;
+    let mut uploaded = 0;
     let mut speed = 0.;
-    video = bili
-        .upload_file(&video.filename, |instant, total, size| {
-            window
-                .emit(
-                    "progress",
-                    (
-                        &video.filename,
-                        total_size as f64 / total as f64 * 100.,
-                        total_size as f64 / 1000. / instant.elapsed().as_millis() as f64,
-                    ),
-                )
-                .unwrap();
-            total_size += size;
-            speed = total_size as f64 / 1000. / instant.elapsed().as_millis() as f64;
-            println!(
-                "{:.2}% => {:.2} MB/s.",
-                total_size as f64 / total as f64 * 100.,
-                speed
-            );
-            println!("{}", remove.load(Ordering::Relaxed));
-            remove.load(Ordering::Relaxed)
-        })
-        .await?;
-    // new_videos.push(video);
-    // }
-
+    let probe = line::Probe::probe().await?;
+    let filename = video.filename;
+    let filepath = PathBuf::from(&filename);
+    let parcel = probe.to_uploader(&filepath).await?;
+    let total_size = parcel.total_size;
+    let instant = Instant::now();
+    video = parcel.upload(&client, |len| {
+        window
+            .emit(
+                "progress",
+                (
+                    &filename,
+                    uploaded as f64 / total_size as f64 * 100.,
+                    uploaded as f64 / 1000. / instant.elapsed().as_millis() as f64,
+                ),
+            )
+            .unwrap();
+        uploaded += len;
+        speed = uploaded as f64 / 1000. / instant.elapsed().as_millis() as f64;
+        println!(
+            "{:.2}% => {:.2} MB/s.",
+            uploaded as f64 / total_size as f64 * 100.,
+            speed
+        );
+        println!("{}", remove.load(Ordering::Relaxed));
+        remove.load(Ordering::Relaxed)
+    }).await?;
     println!("上传成功");
     Ok((video, speed))
 }
 
 #[tauri::command]
 async fn submit(studio: Studio) -> Result<serde_json::Value> {
-    let mut client = Client::new();
-    let login_info = client
+    let login_info = Client::new()
         .login_by_cookies(std::fs::File::open("cookies.json")?)
         .await?;
-    let bili = BiliBili::new((client, login_info));
-    let mut bilibili = bili.submit(studio).await?;
-    Ok(bilibili)
+    let ret = studio.submit(&login_info).await?;
+    // let bili = BiliBili::new((client, login_info));
+    // let mut bilibili = bili.submit(studio).await?;
+    Ok(ret)
 }
 
 #[tauri::command]
 async fn archive_pre() -> Result<serde_json::Value> {
-    let mut client = Client::new();
+    let client = Client::new();
     let login_info = client
         .login_by_cookies(std::fs::File::open("cookies.json")?)
         .await?;
-    let bili = BiliBili::new((client, login_info));
-    let mut bilibili = bili.archive_pre().await?;
-    Ok(bilibili)
+    let bili = BiliBili::new(login_info, client).await;
+    Ok(bili.archive_pre().await?)
 }
 
 #[tauri::command]
