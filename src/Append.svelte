d32bd04f385871a53abd990f6462371395651345
woclass <git@wo-class.cn>
2023-01-27 20:12:34 +0800

    a11y: fix bv upload page

--- src/Append.svelte
+++ src/Append.svelte
@@ -48,12 +48,12 @@
             <label class="swap swap-rotate ml-2" on:click={sortVideos}>
 
                 <!-- this hidden checkbox controls the state -->
-                <input type="checkbox" bind:checked="{order}"/>
+                <input aria-label="排序视频" type="checkbox" bind:checked="{order}" />
 
-                <svg xmlns="http://www.w3.org/2000/svg" class="swap-off fill-current h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" class="swap-off fill-current h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4" />
                 </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" class="swap-on fill-current h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" class="swap-on fill-current h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4" />
                 </svg>
             </label>
--- src/Progress.svelte
+++ src/Progress.svelte
@@ -52,12 +52,14 @@
         <!--        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z" />-->
         <!--    </svg>-->
         <!--{:else }-->
-        <svg class="svg m-auto h-7 w-7" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+        <svg aria-hidden="true" class="svg m-auto h-7 w-7" fill="none" stroke="currentColor" viewBox="0 0 24 24"
              xmlns="http://www.w3.org/2000/svg">
             <path d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z" stroke-linecap="round" stroke-linejoin="round"
                   stroke-width="2"/>
         </svg>
-        <svg class="del animate-bounce m-auto h-7 w-7" fill="currentColor" on:click={remove}
+        <svg class="del animate-bounce m-auto h-7 w-7" fill="currentColor"
+             aria-label="删除文件"
+             on:click={remove} on:keyup={remove}
              viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path clip-rule="evenodd"
                   d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
@@ -70,22 +72,22 @@
         <div class="flex">
             <span class="w-full">
                 <div class="flex w-full justify-between">
-                    <input bind:value="{file.title}" class="bg-inherit w-full truncate"/>
+                    <input aria-label="文件名" bind:value="{file.title}" class="bg-inherit w-full truncate"/>
                     <!--{title}-->
-                    <div class="text-gray-500 min-w-fit text-sm">{(totalSize/1024/1024).toFixed(2)} MiB</div>
+                    <div aria-label="文件大小" class="text-gray-500 min-w-fit text-sm">{(totalSize/1024/1024).toFixed(2)} MiB</div>
                 </div>
-                <span class="block max bg-yellow-300 border-yellow-300 border-opacity-60 border rounded-full" class:complete={!complete}
+                <span aria-hidden="true" class="block max bg-yellow-300 border-yellow-300 border-opacity-60 border rounded-full" class:complete={!complete}
                       style="width: {$fitProgress}%;"></span>
             </span>
         </div>
     </div>
     <div class="flex-none flex flex-col w-20 h-12 justify-center items-center text-gray-500 font-mono text-xs">
         <!-- This item will not grow -->
-        <div>
+        <div aria-label="上传速度">
             <!--{Math.round($fitSpeed * 100) / 100} MB/s-->
             {$fitSpeed.toFixed(2)} MB/s
         </div>
-        <div>
+        <div aria-label="上传进度">
             {$fitProgress.toFixed(2)} %
         </div>
     </div>
