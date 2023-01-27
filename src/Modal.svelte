058e5e2e831f01d7d32d8006624b837996993458
woclass <git@wo-class.cn>
2023-01-27 17:48:31 +0800

    a11y: set Modal checkbox type as button

--- src/Modal.svelte
+++ src/Modal.svelte
@@ -20,15 +20,18 @@
 </label>
 
 <!-- Put this part before </body> tag -->
+<!-- 实际的 checkbox 与弹窗 -->
 <div bind:this={topDiv}>
-    <input type="checkbox" id="component-modal-{componentId}" class="modal-toggle">
+    <input type="checkbox" id="component-modal-{componentId}" 
+        role="button"
+        class="modal-toggle">
     <label for="component-modal-{componentId}" class="modal cursor-pointer">
         <label class="modal-box">
             <slot name="box" componentId="component-modal-{componentId}">
+                <!-- 默认的显示内容 -->
                 <h3 class="text-lg font-bold">Congratulations random Interner user!</h3>
                 <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
             </slot>
         </label>
     </label>
 </div>
-
